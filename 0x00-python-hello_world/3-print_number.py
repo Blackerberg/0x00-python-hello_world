7123e29656170e1544cca56d19d3d96e67c3fdc2
Blackerberg <trendy4zeal@yahoo.com>
2022-06-29 01:55:12 +0100

    working source code

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-f'{}[D98}, Battery street[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D["[C[C[C[C[[3~[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C")[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dprint(f"{98} Battery street")
+print("{:d} Battery street".format(number))
